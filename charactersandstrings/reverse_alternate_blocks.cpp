85761489c9d4deb49feee1cea7f909d9f8958e03
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-26 11:37:43 +0530

    string problems

---
+++ charactersandstrings/reverse_alternate_blocks.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string rev(string s,int k){
+    int p=1;
+    int n=s.size();
+    for(int i=0;i<n;i+=k){
+        int start=i;
+        int end=min(i+k-1,n-1);
+        if(p%2==0) std::reverse(s.begin()+i,s.begin()+i+k);
+        p++;
+    }
+    return s;
+       
+}
+int main() {
+    string input;
+    getline(cin,input);
+    int k;
+    cin>>k;
+    string res=rev(input,k);
+    cout<<res;
+    return 0;
+}
